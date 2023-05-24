0afb29988f50b33739d4589f3fe1b9b1cda47de5
Szymon Gatner <szymon.gatner@vox.pl>
2023-05-24 15:41:34 +0200

    more overloads for TryFirst()

--- Packages/UnityFunctional/Runtime/CollectionsGeneric.cs
+++ Packages/UnityFunctional/Runtime/CollectionsGeneric.cs
@@ -6,15 +6,6 @@ namespace Bravasoft.Functional
 {
     public static class CollectionsGeneric
     {
-        public static Option<T> TryFirst<T>(this List<T> list, Func<T, bool> predicate)
-        {
-            for (int i = 0; i < list.Count; ++i)
-                if (predicate(list[i]))
-                    return list[i];
-
-            return Option<T>.None;
-        }
-
         public static Option<TValue> TryGetValue<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dict, TKey key) =>
             dict.TryGetValue(key, out var value) ? Option.Some(value) : Option.None;
 
