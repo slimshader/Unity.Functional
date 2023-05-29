f0a353198392936ab03d42ef91e899c12925a1f1
Szymon Gatner <szymon.gatner@gmail.com>
2023-05-29 21:01:30 +0200

    more Physics.Raycast and GameObject methods wrapped

--- Packages/UnityFunctional/Runtime/Unity/GameObjectExtensions.cs
+++ Packages/UnityFunctional/Runtime/Unity/GameObjectExtensions.cs
@@ -1,16 +1,14 @@
-﻿using UnityEngine;
-
-namespace Bravasoft.Functional.Unity
+﻿namespace Bravasoft.Functional.Unity
 {
     public static class GameObjectExtensions
     {
-        public static Option<T> TryGetComponent<T>(this GameObject go) =>
+        public static Option<T> TryGetComponent<T>(this UnityEngine.GameObject go) =>
             go.TryGetComponent(out T c) ? Option.Some(c) : Option.None;
 
-        public static Option<T> TryGetComponentInParent<T>(this GameObject go) =>
+        public static Option<T> TryGetComponentInParent<T>(this UnityEngine.GameObject go) =>
             Prelude.Optional(go.GetComponentInParent<T>());
 
-        public static Option<T> TryGetComponentInChildren<T>(this GameObject go) =>
+        public static Option<T> TryGetComponentInChildren<T>(this UnityEngine.GameObject go) =>
             Prelude.Optional(go.GetComponentInChildren<T>());
     }
 
--- Packages/UnityFunctional/Runtime/Unity/Physics.cs
+++ Packages/UnityFunctional/Runtime/Unity/Physics.cs
@@ -6,6 +6,23 @@ namespace Bravasoft.Functional.Unity
     {
         public static Option<RaycastHit> TryRaycast(in Ray ray) =>
             UnityEngine.Physics.Raycast(ray, out RaycastHit hit) ? Option.Some(hit) : Option.None;
-    }
 
+        public static Option<RaycastHit> TryRaycast(in Ray ray, float maxDistance) =>
+            UnityEngine.Physics.Raycast(ray, out RaycastHit hit, maxDistance) ? Option.Some(hit) : Option.None;
+
+        public static Option<RaycastHit> TryRaycast(in Ray ray, float maxDistance, int layerMask) =>
+            UnityEngine.Physics.Raycast(ray, out RaycastHit hit, maxDistance, layerMask) ? Option.Some(hit) : Option.None;
+
+        public static Option<RaycastHit> TryRaycast(in Ray ray, float maxDistance, int layerMask, QueryTriggerInteraction queryTriggerInteraction) =>
+            UnityEngine.Physics.Raycast(ray, out RaycastHit hit, maxDistance, layerMask, queryTriggerInteraction) ? Option.Some(hit) : Option.None;
+
+        public static Option<RaycastHit> TryRaycast(Vector3 origin, Vector3 direction, float maxDistance, int layerMask) =>
+            UnityEngine.Physics.Raycast(origin, direction, out RaycastHit hit, maxDistance, layerMask) ? Option.Some(hit) : Option.None;
+
+        public static Option<RaycastHit> TryRaycast(Vector3 origin, Vector3 direction, float maxDistance) =>
+            UnityEngine.Physics.Raycast(origin, direction, out RaycastHit hit, maxDistance) ? Option.Some(hit) : Option.None;
+
+        public static Option<RaycastHit> TryRaycast(Vector3 origin, Vector3 direction) =>
+            UnityEngine.Physics.Raycast(origin, direction, out RaycastHit hit) ? Option.Some(hit) : Option.None;
+    }
 }
