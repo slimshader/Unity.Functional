6d317c4bc35ce4f574e27a8c80e7ea4fe6fb2f90
Szymon Gatner <szymon.gatner@gmail.com>
2024-02-06 20:05:17 +0100

    testing async result

---
+++ Packages/UnityFunctionalAsync/Runtime/AsyncResult.cs
@@ -0,0 +1,67 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+
+namespace Bravasoft.Functional
+{
+    public static partial class AsyncPrelude
+    {
+        public static UniTask<Result<T>> AsyncOk<T>(T value) => UniTask.FromResult(Result<T>.Ok(value));
+        public static UniTask<Result<T>> AsyncFail<T>(string errorMessage) => UniTask.FromResult(Result<T>.Fail(new Error(errorMessage)));
+    }
+    public static class AsyncResult
+    {
+        public static async UniTask<Result<U>> Map<T, U>(this UniTask<Result<T>> asyncResult, Func<T, U> map)
+        {
+            try
+            {
+                var t = await asyncResult;
+                return t.Map(map);
+            }
+            catch (Exception ex)
+            {
+                return Result<U>.Fail(ex);
+            }
+        }
+
+        public static async UniTask<Result<U>> Bind<T, U>(this UniTask<Result<T>> asyncResult, Func<T, UniTask<Result<U>>> bind)
+        {
+            try
+            {
+                var (isOk, value, error) = await asyncResult;
+
+                if (isOk)
+                {
+                    return await bind(value);
+                }
+
+                return Result<U>.Fail(error);
+            }
+            catch (Exception ex)
+            {
+                return Result<U>.Fail(ex);
+            }
+        }
+
+
+        public static async UniTask<Result<T>> Catch<T>(this UniTask<Result<T>> task)
+        {
+            try
+            {
+                return await task;
+            }
+            catch (Exception ex)
+            {
+                return Result<T>.Fail(ex);
+            }
+        }
+
+        // LINQ
+
+        public static UniTask<Result<U>> Select<T, U>(this UniTask<Result<T>> task, Func<T, U> selector) =>
+            task.Map(selector);
+
+
+        public static UniTask<Result<U>> SelectMany<T, T1, U>(this UniTask<Result<T>> task, Func<T, UniTask<Result<T1>>> selector, Func<T, T1, U> resultSelector) =>
+            task.Bind(t => selector(t).Map(t1 => resultSelector(t, t1)));
+    }
+}
--- Packages/UnityFunctionalAsync/Tests/AsyncTests.cs
+++ Packages/UnityFunctionalAsync/Tests/AsyncTests.cs
@@ -1,88 +1,88 @@
-using Cysharp.Threading.Tasks;
-using NUnit.Framework;
-using System.Collections;
-using UnityEngine.TestTools;
+//using Cysharp.Threading.Tasks;
+//using NUnit.Framework;
+//using System.Collections;
+//using UnityEngine.TestTools;
 
 
-namespace Bravasoft.Functional.Tests
-{
-    public class AsyncTests
-    {
-        [UnityTest]
-        public IEnumerator ResultHasValueWhenTaskIsSuccessful()
-        {
-            async UniTask<int> AsyncInt(int value)
-            {
-                await UniTask.Delay(100);
-                return value;
-            }
+//namespace Bravasoft.Functional.Tests
+//{
+//    public class AsyncTests
+//    {
+//        [UnityTest]
+//        public IEnumerator ResultHasValueWhenTaskIsSuccessful()
+//        {
+//            async UniTask<int> AsyncInt(int value)
+//            {
+//                await UniTask.Delay(100);
+//                return value;
+//            }
 
-            Result<int> result = default;
+//            Result<int> result = default;
 
-            var task = AsyncInt(1).ToAsyncResult().ContinueWith(x => result = x);
+//            var task = AsyncInt(1).CatchResult().ContinueWith(x => result = x);
 
-            yield return task.ToCoroutine();
+//            yield return task.ToCoroutine();
 
-            Assert.IsTrue(result);
-        }
+//            Assert.IsTrue(result);
+//        }
 
-        [UnityTest]
-        public IEnumerator ResultHasErrorWhenTaskIsFaulted()
-        {
-            async UniTask<int> AsyncInt(int value)
-            {
-                await UniTask.Delay(100);
-                throw new System.Exception();
-            }
+//        [UnityTest]
+//        public IEnumerator ResultHasErrorWhenTaskIsFaulted()
+//        {
+//            async UniTask<int> AsyncInt(int value)
+//            {
+//                await UniTask.Delay(100);
+//                throw new System.Exception();
+//            }
 
-            Result<int> result = default;
+//            Result<int> result = default;
 
-            var task = AsyncInt(1).ToAsyncResult().ContinueWith(x => result = x);
+//            var task = AsyncInt(1).CatchResult().ContinueWith(x => result = x);
 
-            yield return task.ToCoroutine();
+//            yield return task.ToCoroutine();
 
-            Assert.IsFalse(result);
-            Assert.IsTrue(result.IsException<System.Exception>());
-        }
+//            Assert.IsFalse(result);
+//            Assert.IsTrue(result.IsException<System.Exception>());
+//        }
 
-        [UnityTest]
-        public IEnumerator CanRetry()
-        {
-            int calls = 0;
+//        [UnityTest]
+//        public IEnumerator CanRetry()
+//        {
+//            int calls = 0;
 
-            async UniTask<int> AsyncInt(int value)
-            {
-                calls++;
-                await UniTask.Delay(100);
-                throw new System.Exception();
-            }
+//            async UniTask<int> AsyncInt(int value)
+//            {
+//                calls++;
+//                await UniTask.Delay(100);
+//                throw new System.Exception();
+//            }
 
-            var task = Async.Retry(() => AsyncInt(1), 3, 1).ToAsyncResult();
+//            var task = UniTask.Retry(() => AsyncInt(1), 3, 1).CatchResult();
 
-            yield return task.ToCoroutine();
+//            yield return task.ToCoroutine();
 
-            Assert.AreEqual(4, calls);
-        }
+//            Assert.AreEqual(4, calls);
+//        }
 
 
-        [UnityTest]
-        public IEnumerator CanRetryResult()
-        {
-            int calls = 0;
+//        [UnityTest]
+//        public IEnumerator CanRetryResult()
+//        {
+//            int calls = 0;
 
-            async UniTask<int> AsyncInt(int value)
-            {
-                calls++;
-                await UniTask.Delay(100);
-                throw new System.Exception();
-            }
+//            async UniTask<int> AsyncInt(int value)
+//            {
+//                calls++;
+//                await Cysharp.Threading.Tasks.UniTask.Delay(100);
+//                throw new System.Exception();
+//            }
 
-            var task = Async.Retry(Async.ToAsyncResult(() => AsyncInt(1)), 3, 1);
+//            var task = UniTask.Retry(UniTask.ToAsyncResult(() => AsyncInt(1)), 3, 1);
 
-            yield return task.ToCoroutine();
+//            yield return task.ToCoroutine();
 
-            Assert.AreEqual(4, calls);
-        }
+//            Assert.AreEqual(4, calls);
+//        }
 
-    }
-}
+//    }
+//}
